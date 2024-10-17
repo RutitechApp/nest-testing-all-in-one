bff3b88dcedd031f75a968dc0a8116c3abbf703c
RutitechApp <rutilanttechnology@gmail.com>
2024-10-17 15:24:07 +0530

    Feat: project setup

--- dist/app.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class AppService {
-    getHello(): {
-        title: string;
-        subtitle: string;
-    };
-}
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): { title: string; subtitle: string } {
-    return {
-      title: 'Dynamic Title from Service',
-      subtitle: 'Dynamic Subtitle from Service',
-    };
+  getHello(): string {
+    return 'Hello World!';
   }
 }
