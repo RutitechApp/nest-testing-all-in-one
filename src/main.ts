7e02dddfaf6e8e2fa342a58f229d62f6188fac64
RutitechApp <rutilanttechnology@gmail.com>
2024-10-17 16:42:27 +0530

    Feat: testing vercel file chnage

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as ejs from 'ejs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
